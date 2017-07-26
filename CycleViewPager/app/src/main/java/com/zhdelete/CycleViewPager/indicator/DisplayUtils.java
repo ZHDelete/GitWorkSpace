7ab56a873fdf83e5ec56ad1d52229e79391c0612
ZHDelete <1414524461@qq.com>
2017-07-26 13:41:25 +0800

    DisplayUtils增加dp px 转换方法

--- CycleViewPager/app/src/main/java/com/zhdelete/CycleViewPager/indicator/DisplayUtils.java
+++ CycleViewPager/app/src/main/java/com/zhdelete/CycleViewPager/indicator/DisplayUtils.java
@@ -9,10 +9,12 @@ import android.util.TypedValue;
 
 public class DisplayUtils {
     public static int dpToPx(int dp) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                Resources.getSystem().getDisplayMetrics());
     }
 
     public static int pxToDp(float px) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px, Resources.getSystem().getDisplayMetrics());
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, px,
+                Resources.getSystem().getDisplayMetrics());
     }
 }
